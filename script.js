e041b0df94ca67254de0a0c08300bcde098f5cea
gortanian <gortanian@gmail.com>
2023-01-18 11:54:58 -0800

    Create library functions
    Library functions include the book constructor function, addBookToLibrary,
    and displayLibrary, along with a simple test in the script.

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-alert("hello there!");
+// TODO
+// - change displayLibrary to put the book info into a div with the class "book"
+
+// Library initialization
+let myLibrary = [];
+
+// object constructor function
+function Book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
+}
+
+// add the info function to Book object prototype
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.read === true ? "already read" : "not read yet"
+  }`;
+};
+
+// Adds book to myLibrary
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+// display library function
+function displayLibrary() {
+  myLibrary.forEach(function (book) {
+    // temporarily, just print the books to the console.
+    console.log(book.info());
+
+    // put the book info into a div with the class "book"
+    const htmlBook = `
+    <div>
+      <h2>${book.title}</h2>
+      <h3>by ${book.author}</h3>
+      <div>${book.pages} pages</div>
+      <div>${book.read ? "Already read" : "Not read yet"}</div>
+    </div>`;
+
+    document.body.innerHTML += htmlBook;
+  });
+}
+
+// Manually enter a few books
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 295, true);
+addBookToLibrary("The Hunger Games", "Suzanne Collins", 384, false);
+addBookToLibrary("The Three-Body Problem", "Liu Cixin", 416, false);
+
+displayLibrary();
